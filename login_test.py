e7a1ea68f88cfa83597f96d4d7104369e40925e8
Erik Holm <shadi_international@yahoo.se>
2023-03-13 21:45:27 +0100

    Made new changes

--- login_test.py
+++ login_test.py
@@ -6,6 +6,8 @@ Description:
 import unittest
 from time import sleep
 
+from packaging import version
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
